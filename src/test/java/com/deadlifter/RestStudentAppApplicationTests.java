eeabdff1b268f1346baca19d287633f4cde4fa4e
Si7H <arkadiusz.mecka@gmail.com>
2022-03-29 00:56:09 +0200

    spring boot template

---
+++ src/test/java/com/deadlifter/RestStudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deadlifter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestStudentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
